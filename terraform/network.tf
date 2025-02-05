7dc0bc4f37ad7349f6e017c7720a318784a4bf1d
yeamoon <yeamoon@users.noreply.github.com>
2025-02-05 17:56:09 -0500

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,8 +1,10 @@
+# Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group
   location = var.location
 }
 
+# Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "dev-vnet"
   location            = azurerm_resource_group.rg.location
@@ -10,6 +12,7 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
 }
 
+# Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "my-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -17,6 +20,7 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+# Public IP Address
 resource "azurerm_public_ip" "public_ip" {
   name                = "dev_public-ip"
   resource_group_name = azurerm_resource_group.rg.name
@@ -24,6 +28,7 @@ resource "azurerm_public_ip" "public_ip" {
   allocation_method   = "Dynamic"
 }
 
+# Network Interface
 resource "azurerm_network_interface" "nic" {
   name                = "dev-nic"
   location            = azurerm_resource_group.rg.location
@@ -35,4 +40,34 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = azurerm_public_ip.public_ip.id
     private_ip_address_allocation = "Dynamic"
   }
+}
+
+# Virtual Machine (Attach to NIC)
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "dev-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
+# Output the public IP address
+output "vm_public_ip" {
+  description = "Public IP address of the virtual machine"
+  value       = azurerm_public_ip.public_ip.ip_address
 }
